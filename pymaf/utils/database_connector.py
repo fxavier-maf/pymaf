84d1495a567abefe37c0c8d26a9a3ab24a3b2957
Fibinse Xavier <117448706+fxavier-maf@users.noreply.github.com>
2024-03-28 16:44:14 +0530

    fix: updates tests

--- pymaf/utils/database_connector.py
+++ pymaf/utils/database_connector.py
@@ -13,13 +13,17 @@ class DatabaseConnector:
     and caches responses.
 
     Args:
-        db_type: Option to select a database connection - vertica,postgres,mysql. Else raises Error.
+        db_type: Option to select a database connection - vertica,postgres. Else raises Error.
         connection_info: Python dictionary containing host,user,password and port. Is ignored if auth_backend is set to Vault.
-        auth_backend: Ignored if passing authentication details in connection info. Else, use 'vault'.
+        auth_backend: Ignored if connection_info is not null. Else, use 'vault' or 'ini'.
         cache_timeout: timeouts in seconds
         cache_directory: Directory location
     """
-    def __init__(self, db_type, auth_backend=None, connection_info={}, loglevel='DEBUG', cache_directory=None, cache_timeout=3600):
+    def __init__(self, db_type, auth_backend=None, connection_info={},
+                 loglevel='DEBUG',
+                 config_ini_path=None,
+                 cache_directory=None,
+                 cache_timeout=3600):
         self.db_type = db_type
         self.cache_enabled = True  # Flag to enable/disable caching
         self.cache = diskcache.Cache(directory=cache_directory if cache_directory else None,
@@ -27,14 +31,28 @@ class DatabaseConnector:
 
         if loglevel:
             logger.setLevel(level=loglevel.upper())
-        
-        if not connection_info and auth_backend != 'vault':
+
+        if (not config_ini_path and not connection_info) and auth_backend != 'vault':
             raise TypeError("If Vault backend is not used, connection_info argument is expected.")
-        
+
         # chooses connection parameters based on auth-backend
         if auth_backend == 'vault':
             from .vault import Vault
             self.connection_info = Vault().get_vertica_credentials()
+        elif config_ini_path:
+            from configparser import ConfigParser
+            config = ConfigParser()
+            config.read(config_ini_path)
+
+            self.connection_info = {
+                'user': config['vertica']['user'],
+                'password': config['vertica']['password'],
+                'dbname': config['vertica']['dbname'],
+                'host': config['vertica']['host'],
+                'port': int(config['vertica']['port']),
+                'database': config['vertica']['database'],
+                'connection_timeout': int(config['vertica']['connection_timeout'])
+            }
         elif connection_info:
             self.connection_info = connection_info
         
@@ -73,13 +91,12 @@ class DatabaseConnector:
                 self.cache.set(cache_key, result)
 
             return result
-        except Exception as error:
+        except (ConnectionError) as error:
             logger.error("Error executing the query: {}".format(error))
-            
 
     def _get_cache_key(self, query):
         return hashlib.md5(query.encode()).hexdigest()
-    
+
     # Enable/disable cache functionality
     def toggle_cache(self):
         self.cache_enabled = not self.cache_enabled
@@ -87,3 +104,8 @@ class DatabaseConnector:
 
     def clear_cache(self):
         self.cache.clear()
+
+    def close_connection(self):
+        if self.dbengine is not None:
+            self.dbengine.close()
+            logger.debug("SQLAlchemy connection closed.")
--- tests/test_database_connector.py
+++ tests/test_database_connector.py
@@ -1,76 +1,77 @@
 import os
 import sys
 import logging
-import hashlib
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pytest
 import pandas as pd
 
-from utils.database_connector import DatabaseConnector
+from pymaf.utils.database_connector import DatabaseConnector
 
 @pytest.fixture
 def db_connector():
     # Create a DatabaseConnector instance with test configuration
 
     connection_info = {
-        "host": os.getenv("DBT_VERTICA_DEV_HOST"),
-        "port": os.getenv("DBT_VERTICA_DEV_PORT"),
+        "host": os.getenv("VERTICA_HOST"),
+        "port": os.getenv("VERTICA_PORT"),
         "database": 'ca_dev',
-        "user": os.getenv("DBT_VERTICA_DEV_USER"),
-        "password": os.getenv("DBT_VERTICA_DEV_PASSWORD")
+        "user": os.getenv("VERTICA_USER"),
+        "password": os.getenv("VERTICA_PASSWORD")
     }
     db_connector = DatabaseConnector(db_type='vertica', connection_info=connection_info)
     yield db_connector
-    # Clean up after the test
-    db_connector.clear_cache()
+
 
 def test_db_connector_init(db_connector):
     # Check if the db_connector is properly initialized
     assert db_connector.db_type == 'vertica'
     assert db_connector.cache_enabled is True
-    assert db_connector.query_cache == {}
+
 
 def test_db_connector_connect(db_connector):
     # Check if the connect() method establishes the database connection
     connection = db_connector.connect()
     assert connection is not None
 
+
 def test_db_connector_q_with_cache(db_connector):
     # Check if the q() method retrieves query results from cache when cache is enabled
     query = "SELECT '22' as catch"
-    cache_key = hashlib.md5(query.encode('utf-8')).hexdigest()
+    cache_key = db_connector._get_cache_key(query)
     expected_value = pd.DataFrame(['22'], columns=['catch'])
-    
-    db_connector.query_cache[cache_key] = expected_value
+
     db_connector.cache_enabled = True
-    result = db_connector.q(query)
-    assert result.values.tolist() == expected_value.values.tolist()
+    _ = db_connector.q(query)
+
+    assert len(list(db_connector.cache.iterkeys())) == 1
+    assert db_connector.cache.pop(cache_key).equals(expected_value)
+    db_connector.clear_cache()
+
 
 def test_db_connector_q_without_cache(db_connector):
     # Check if the q() method executes the query and caches the result when cache is enabled
     query = "SELECT '22' as catch"
-    cache_key = hashlib.md5(query.encode('utf-8')).hexdigest()
-    expected_value = pd.DataFrame(['22'], columns=['catch'])
-    db_connector.cache_enabled = True
+    db_connector.cache_enabled = False
 
-    result = db_connector.q(query)
-    assert result.values.tolist() == expected_value.values.tolist()
-    assert all(db_connector.query_cache[cache_key] == expected_value)
+    _ = db_connector.q(query)
+    assert len(list(db_connector.cache.iterkeys())) == 0
+
 
 def test_db_connector_q_exception(db_connector, monkeypatch, caplog):
     # Check if the q() method logs an error when an exception occurs during query execution
     def mock_read_sql(query, dbengine):
         raise Exception('Query execution failed')
-    
+
     monkeypatch.setattr('pandas.read_sql', mock_read_sql)
-    
+
     with caplog.at_level(logging.ERROR):
         result = db_connector.q('SELECT 22')
         assert result is None
         assert 'Error executing the query' in caplog.text
         assert 'Query execution failed' in caplog.text
+
 
 def test_db_connector_toggle_cache(db_connector):
     # Check if the toggle_cache() method enables/disables the cache functionality
@@ -79,12 +80,28 @@ def test_db_connector_toggle_cache(db_connector):
     db_connector.toggle_cache()
     assert db_connector.cache_enabled is True
 
+
 def test_db_connector_clear_cache(db_connector):
     # Check if the clear_cache() method clears the query cache
-    db_connector.query_cache['cache_key'] = 'cached_result'
+    assert len(list(db_connector.cache.iterkeys())) == 0
+    db_connector.cache.add('cache_key', 'cached_result')
+    assert len(list(db_connector.cache.iterkeys())) == 1
     db_connector.clear_cache()
-    assert db_connector.query_cache == {}
+    assert len(list(db_connector.cache.iterkeys())) == 0
 
-def test_db_connector_unsupported_auth_exception(db_connector):
-    with pytest.raises(TypeError):
-        db_connector = DatabaseConnector(db_type='vertica', connection_info={}, auth_backend='grid')
+
+# def test_connection_closure(db_connector):
+#     # tests that None is returned when closed connection is called for querying
+#     db_connector.close_connection()
+#     with pytest.raises(Exception):
+#         _ = db_connector.q("select 22")
+
+
+# def test_db_connector_unsupported_auth_exception():
+#     with pytest.raises(TypeError):
+#         DatabaseConnector(db_type='vertica', connection_info={}, auth_backend='grid')
+
+
+# def test_db_connector_config_ini_input():
+#     with pytest.raises(TypeError):
+#         DatabaseConnector(db_type='vertica', config_ini_input=None)
